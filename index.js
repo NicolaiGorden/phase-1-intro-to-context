1ddca1bf1d457890e6a95ce0e611968488a3fe8a
Nicolai Gorden <nicolaigorden@gmail.com>
2022-03-16 10:42:12 -0500

    commit tree febd080cfd9f043d8faeb939395068a357c72c93

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Your code here
+const createEmployeeRecord = function(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+const createEmployeeRecords = function(arrays){
+    let records = []
+    arrays.map(e => records.push(createEmployeeRecord(e)))
+    return records
+}
+
+const createTimeInEvent = function(record,date){
+    let timeArr = date.split(' ')
+    record.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(timeArr[1]),
+        date: timeArr[0],
+    })
+    return record
+}
+
+const createTimeOutEvent = function(record,date){
+    let timeArr = date.split(' ')
+    record.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(timeArr[1]),
+        date: timeArr[0],
+    })
+    return record
+}
+
+const hoursWorkedOnDate = function(record,date){
+    const timeIn = record.timeInEvents.find(o => o.date === date).hour
+    const timeOut = record.timeOutEvents.find(o => o.date === date).hour
+    return (timeOut - timeIn)/100
+}
+
+const wagesEarnedOnDate = function(record,date){
+    return hoursWorkedOnDate(record,date)*record.payPerHour
+}
+
+const allWagesFor = function(record){
+    let datesWorked = []
+    //array of dates worked
+    let wagesEarned = []
+    //iterate over all dates worked and run wagesEarnedOnDate for them, push all results into wagesEarned, reduce.
+    record.timeInEvents.map(e => datesWorked.push(e.date))
+    datesWorked.map(e => wagesEarned.push(wagesEarnedOnDate(record, e)))
+    return wagesEarned.reduce((a,c)=>a+c)
+}
+
+const calculatePayroll = function(array){
+    let allWagesEarned = []
+    array.map(e => allWagesEarned.push(allWagesFor(e)))
+    return allWagesEarned.reduce((a,c)=>a+c)
+}
